37bbfb0ffc2c770672014058b160a1f04988f31f
Simon Hylleberg <simonhylleberg@gmail.com>
2018-10-03 19:58:25 +0200

    kage

---
+++ Ex11-Temperature/Temperature.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11_Temperature
+{
+
+
+    public class Temperature
+    {
+        public enum TemperatureScale
+        {
+            Celsius,
+            Fahrenheit
+        }
+        public double Fahrenheit {
+            get { return temperature * 9 / 5 + 32; }
+            set { temperature = (value - 32) * 5 / 9; }
+        }
+        public double Celsius
+        {
+            get { return temperature; }
+            set { temperature = value; }
+        }
+
+        public void SetTemperature(double v, TemperatureScale u)
+        {
+            switch (u)
+            {
+                case TemperatureScale.Fahrenheit:
+                    Fahrenheit = v;
+                    break;
+                default:
+                    Celsius = v;
+                    break;
+            }
+        }
+        private double temperature;
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ff3873e-d0d7-4e9d-8714-816c63753944")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
